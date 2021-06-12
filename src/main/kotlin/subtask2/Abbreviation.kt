f96a989ab1f144c92940bb7864fd0f3eca41bf78
Salim <vsalim1998@gmail.com>
2021-06-12 21:35:41 +0300

    commit

--- src/main/kotlin/subtask2/Abbreviation.kt
+++ src/main/kotlin/subtask2/Abbreviation.kt
@@ -4,7 +4,23 @@ class Abbreviation {
 
     // TODO: Complete the following function
     fun abbreviationFromA(a: String, b: String): String {
-        return if (a.toUpperCase().contains(b.toUpperCase())) {
+        val aArr = arrayListOf<Int>()
+        var a2 = ""
+        val bArr = arrayListOf<Int>()
+        var b2 = ""
+        for (i in a.indices) {
+            for (j in b.indices) {
+                if (a[i].equals(b[j], ignoreCase = true)) {
+                    aArr.add(i)
+                    a2 += a[i]
+                    bArr.add(j)
+                    b2 += b[j]
+                }
+            }
+        }
+        val a1 = aArr.sorted()
+        val b1 = bArr.sorted()
+        return if (a1.equals(aArr) && b1.equals(bArr) && b.toUpperCase().equals(b2.toUpperCase())) {
             "YES"
         } else {
             "NO"
